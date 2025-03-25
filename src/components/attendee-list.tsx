a08323a78992c690e0de6bcd06d15088fe997cd8
LucasAmadio1 <lucasrochaamadio02@gmail.com>
2025-03-24 23:28:37 -0300

    components

--- src/components/attendee-list.tsx
+++ src/components/attendee-list.tsx
@@ -1,3 +1,7 @@
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
+import 'dayjs/locale/pt-br'
+
 import {
   ChevronLeft,
   ChevronRight,
@@ -6,13 +10,43 @@ import {
   MoreHorizontal,
   Search,
 } from 'lucide-react'
+import { type ChangeEvent, useState } from 'react'
+import { attendees } from '../data/attendees'
 import { IconButton } from './icon-button'
 import { Table } from './table/table'
 import { TableCell } from './table/table-cell'
 import { TableHeader } from './table/table-header'
 import { TableRow } from './table/table-row'
 
+dayjs.extend(relativeTime)
+dayjs.locale('pt-br')
+
 export function AttendeeList() {
+  const [search, setSearch] = useState('')
+  const [page, setPage] = useState(1)
+
+  const totalPages = Math.ceil(attendees.length / 10)
+
+  function onSearchInputChanged(event: ChangeEvent<HTMLInputElement>) {
+    setSearch(event.target.value)
+  }
+
+  function goToNextPage() {
+    setPage(page + 1)
+  }
+
+  function goToPreviousPage() {
+    setPage(page - 1)
+  }
+
+  function goToFirstPage() {
+    setPage(1)
+  }
+
+  function goToLastPage() {
+    setPage(totalPages)
+  }
+
   return (
     <div className="flex flex-col gap-4">
       <div className="flex gap-3 items-center">
@@ -21,6 +55,7 @@ export function AttendeeList() {
         <div className="px-3 py-1.5 border border-white/10 rounded-lg w-72 flex items-center gap-3">
           <Search className="size-4 text-emerald-300" />
           <input
+            onChange={onSearchInputChanged}
             className="bg-transparent flex-1 outline-none h-auto border-0 p-0 text-sm ring-0"
             placeholder="Buscar participante"
           />
@@ -44,26 +79,26 @@ export function AttendeeList() {
           </tr>
         </thead>
         <tbody>
-          {Array.from({ length: 8 }).map((_, i) => {
+          {attendees.slice((page - 1) * 10, page * 10).map((attendee) => {
             return (
-              <TableRow key={i}>
+              <TableRow key={attendee.id}>
                 <TableCell>
                   <input
                     className="size-4 bg-black/20 rounded border border-white/10 "
                     type="checkbox"
                   />
                 </TableCell>
-                <TableCell>1231231</TableCell>
+                <TableCell>{attendee.id}</TableCell>
                 <TableCell>
                   <div className="flex flex-col gap-1">
                     <span className="font-semibold text-white">
-                      Lucas Rocha Amadio
+                      {attendee.name}
                     </span>
-                    <span>lucasrochaamadio02@gmail.com</span>
+                    <span>{attendee.email}</span>
                   </div>
                 </TableCell>
-                <TableCell>7 dias atrás</TableCell>
-                <TableCell>3 dias atrás</TableCell>
+                <TableCell>{dayjs().to(attendee.createdAt)}</TableCell>
+                <TableCell>{dayjs().to(attendee.checkedInAt)}</TableCell>
                 <TableCell>
                   <IconButton transparent>
                     <MoreHorizontal className="size-4" />
@@ -75,24 +110,34 @@ export function AttendeeList() {
         </tbody>
         <tfoot>
           <tr>
-            <TableCell colSpan={3}>Mostrando 10 de 228 itens</TableCell>
+            <TableCell colSpan={3}>
+              Mostrando 10 de {attendees.length} itens
+            </TableCell>
             <TableCell className="text-right" colSpan={3}>
               <div className="inline-flex items-center gap-8">
-                <span>Página 1 de 23</span>
+                <span>
+                  Página {page} de {totalPages}
+                </span>
                 <div className="flex gap-1.5">
-                  <IconButton>
+                  <IconButton onClick={goToFirstPage} disabled={page === 1}>
                     <ChevronsLeft className="size-4" />
                   </IconButton>
 
-                  <IconButton>
+                  <IconButton onClick={goToPreviousPage} disabled={page === 1}>
                     <ChevronLeft className="size-4" />
                   </IconButton>
 
-                  <IconButton>
+                  <IconButton
+                    onClick={goToNextPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronRight className="size-4" />
                   </IconButton>
 
-                  <IconButton>
+                  <IconButton
+                    onClick={goToLastPage}
+                    disabled={page === totalPages}
+                  >
                     <ChevronsRight className="size-4" />
                   </IconButton>
                 </div>
--- src/components/table/table-header.tsx
+++ src/components/table/table-header.tsx
@@ -3,9 +3,5 @@ import type { ComponentProps } from 'react'
 interface TableHeaderProps extends ComponentProps<'th'> {}
 
 export function TableHeader({ ...props }: TableHeaderProps) {
-  return (
-    <th {...props} className="py-3 px-4 text-sm font-semibold text-left">
-      Código
-    </th>
-  )
+  return <th {...props} className="py-3 px-4 text-sm font-semibold text-left" />
 }
